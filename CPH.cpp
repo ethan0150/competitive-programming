b5113a860c6edefd46337dcd36e030df640478dd
ethan0150 <minghong2000@gmail.com>
2017-04-16 23:03:46 +0800

    C&P&H template

---
+++ CPH.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define FOR(n) for(ull i=0;i<n;i++)
+#define JFOR(n) for(ull j=0;j<n;j++)
+#define KFOR(n) for(ull k=0;k<n;k++)
+#define ioopt ios_base::sync_with_stdio(0);cin.tie(0);
+//Don't use macros below if U R using ioopt!!
+#define RI(n) scanf("%d",&n)
+#define RUI(n) scanf("%u",&n)
+#define RS(n) scanf("%s",&n)
+#define RC(n) scanf("%c",&n)
+#define PI(n) prullf("%d",n)
+#define PIln(n) prullf("%d\n",n)
+#define PIb(n) prullf("%d ",n)
+#define PS(n) prullf("%s",n)
+#define PSln(n) prullf("%s\n",n)
+using namespace std;
+typedef unsigned long long ull;
+ull product(ull i,ull j){
+    ull ret=1;
+    for(;i<=j;i++)ret*=i;
+    return ret;
+}
+ull C(ull n,ull k){
+    if(k>n-k)return product(k+1,n)/product(1,n-k);
+    else return product(n-k+1,n)/product(1,n);
+}
+ull P(ull n,ull k){return product(n-k+1,n);}
+ull H(ull n,ull k){return C(n+k-1,k);}
+int main(){ioopt
+    return 0;
+}
