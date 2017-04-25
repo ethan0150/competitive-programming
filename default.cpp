8f041e295df3a93bfd7abfbc88fce368f2c80039
ethan0150 <minghong2000@gmail.com>
2017-04-25 23:32:48 +0800

    Nothing more than default code:D
    Of course this doesn't support TCFSHOJ :((

---
+++ default.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define FOR(n) for(int i=0;i<n;i++)
+#define JFOR(n) for(int j=0;j<n;j++)
+#define KFOR(n) for(int k=0;k<n;k++)
+#define ioopt ios_base::sync_with_stdio(0);cin.tie(0);
+#define F first
+#define S second
+//Don't use macros below if U R using ioopt!!
+#define RI(n) scanf("%d",&n)
+#define RUI(n) scanf("%u",&n)
+#define RS(n) scanf("%s",&n)
+#define RC(n) scanf("%c",&n)
+#define PI(n) printf("%d",n)
+#define PIln(n) printf("%d\n",n)
+#define PIb(n) printf("%d ",n)
+#define PS(n) printf("%s",n)
+#define PSln(n) printf("%s\n",n)
+using namespace std;
+typedef pair<int,int> pii;
+int main(){//ioopt
+    return 0;
+}
